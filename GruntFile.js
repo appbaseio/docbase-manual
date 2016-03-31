4d1d24ab9f435c0e80b7d0b885b3336650aaa6df
Siddharth Kothari <sids.aquarius@gmail.com>
2016-03-31 16:59:37 +0530

    update docbase config.js file

---
+++ GruntFile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		docbase: {
+			def: {
+				options: {
+					generatePath: "docs_html/",
+					generateSearchIndex: true,
+					generateHtml: true,
+					baseUrl: "./",
+					operation: 'parallel',
+					urlToAccess: "http://localhost:9001/",
+					assets: ['bower_components', 'styles', 'images', 'docbase-config.js'],
+					checkLoadedSelector: '#navbar-collapse',
+					endDocument: "<script>$(function(){  $('.search-form').searchAppbase('./search-index.json', true); $(document).ready(function(){ setTimeout(function(){ $('#folder-navbar').megaMenu(); },200); }); });</script></html>"
+				}
+			},
+			spa: {
+				options: {
+					onlysearchIndex: true,
+					generatePath: "spa/",
+					generateSearchIndex : true,
+					generateHtml : false,
+					baseUrl: "./",
+					operation: 'parallel',
+					urlToAccess: "http://localhost:9001/",
+					assets: ['docs', 'html', 'index.html', 'bower_components', 'styles', 'images', 'docbase-config.js', 'search-index.json' ],
+					checkLoadedSelector : '#navbar-collapse'
+				}
+			}
+		},
+		connect: {
+			server: {
+				options: {
+					port: 9001,
+					base: './',
+					protocol: 'http'
+				}
+			}
+		},
+		'gh-pages': {
+			def: {
+				options: {
+					base: 'docs_html',
+					user: {
+						name: 'Docbase bot',
+						email: 'awesome@docba.se'
+					},
+					repo: 'https://' + new Buffer(process.env.DOCBASE_TOKEN, 'base64').toString() + '@github.com/appbaseio/docbase-manual.git',
+					message: 'publish gh-pages (auto)',
+					silent: false,
+				},
+				src: ['**']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-docbase');
+	grunt.loadNpmTasks('grunt-gh-pages');
+	
+	// Default task.
+	
+	var target = grunt.option('target') || 'def';
+	
+	grunt.registerTask('default', ['connect', 'docbase:'+target]);
+	grunt.registerTask('publish', ['connect', 'docbase:'+target, 'gh-pages']);
+	grunt.registerTask('spa', ['connect', 'docbase:spa']);
+	
+};
--- docbase-config.js
+++ docbase-config.js
@@ -12,57 +12,72 @@ var docbaseConfig = {
     "path": "manual"
   },
   "github": {
-    "user": "",
-    "repo": "",
-    "path": "",
-    "branch": "",
-    "access_token": ""
+    "user": "appbaseio",
+    "repo": "docbase-manual",
+    "path": "manual",
+    "branch": "master",
+    "access_token": "YjFiZWY3NWYwYzQ4MmEwMjU4NmFiNGRkMGU4NjY3NDA5YWM3ZDdjZQ=="
   },
   "indexHtml": "./html/main.html",
   "flatdocHtml": "./bower_components/docbase/html/flatdoc.html",
   "html5mode": false,
   "default_version": null,
   "versions" : {
-    "v1": [{
+  "v1": [
+    {
       "name": "getting-started",
       "label": "Getting Started",
-      "files": [{
-        "name": "start",
-        "label": "Quick Start"
-      }, {
-        "name": "configure",
-        "label": "Configuration Options"
-      }]
-    }, {
+      "files": [
+        {
+          "name": "start",
+          "label": "Quick Start"
+        },
+        {
+          "name": "configure",
+          "label": "Configuration Options"
+        }
+      ]
+    },
+    {
       "name": "features",
       "label": "Features",
-      "files": [{
-        "name": "search",
-        "label": "Search"
-      }, {
-        "name": "colors",
-        "label": "Colorful"
-      }, {
-        "name": "gh-pages",
-        "label": "Deploy to Github"
-      }, {
-        "name": "versions",
-        "label": "Versatile Navigation"
-      }]
-    }, {
+      "files": [
+        {
+          "name": "search",
+          "label": "Search"
+        },
+        {
+          "name": "colors",
+          "label": "Colorful"
+        },
+        {
+          "name": "gh-pages",
+          "label": "Deploy to Github"
+        },
+        {
+          "name": "versions",
+          "label": "Versatile Navigation"
+        }
+      ]
+    },
+    {
       "name": "layout",
       "label": "Layout",
-      "files": [{
-        "name": "navigation",
-        "label": "Site Navigation"
-      }, {
-        "name": "3col",
-        "label": "Three columns"
-      }, {
-        "name": "markdown",
-        "label": "Markdown Options"
-      }
-     ]
-    }]
-  }
+      "files": [
+        {
+          "name": "navigation",
+          "label": "Site Navigation"
+        },
+        {
+          "name": "3col",
+          "label": "Three columns"
+        },
+        {
+          "name": "markdown",
+          "label": "Markdown Options"
+        }
+      ]
+    }
+  ]
+}
 }
