3ef6085f976bee8019a1369de2a923c974e6c8f4
Siddharth Kothari <sids.aquarius@gmail.com>
2016-04-02 13:41:07 +0530

    Update docbase-config.js

--- docbase-config.js
+++ docbase-config.js
@@ -16,7 +16,7 @@ var docbaseConfig = {
   },
   "indexHtml": "./html/main.html",
   "flatdocHtml": "./bower_components/docbase/html/flatdoc.html",
-  "html5mode": true,
+  "html5mode": false,
   "default_version": "",
   "manual_override": true,
   "versions" : {
